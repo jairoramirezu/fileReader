7aa0c7c2807241208232d3265b1e08a4574129b3
Jairo Ramirez <jairoramirezu@gmail.com>
2022-11-25 15:30:04 -0400

    some changes to load multiples files

--- fileReader.js
+++ fileReader.js
@@ -1,21 +1,21 @@
 const DropArea = document.querySelector(".dropArea");
+const BarLoad = document.querySelector(".barLoad");
 const Result = document.querySelector(".result");
 
 DropArea.addEventListener("dragover", e => {
   e.preventDefault();
-  changeStyle(e.target, "#BF0426");
+  changeStyle(e.target, "blue");
 })
 
 DropArea.addEventListener("dragleave", e => {
   e.preventDefault();
-  changeStyle(e.target,"#172D44");
+  changeStyle(e.target,"#BF0426");
 })
 
 DropArea.addEventListener("drop", e => {
   e.preventDefault();
-  changeStyle(e.target,"#3A8891");
-  loadFile(e.dataTransfer.files[0]);
-  DropArea.innerHTML = "¡Files dropped!";
+  changeStyle(e.target,"blue");
+  loadFile(e.dataTransfer.files);
 })
 
 const changeStyle = (obj, color) => {
@@ -24,31 +24,48 @@ const changeStyle = (obj, color) => {
 }
 
 const loadFile = ar => {
-  const reader = new FileReader();
-  if (ar.type.includes("text")) {
-    reader.readAsText(ar);
-    reader.addEventListener("load", e => {
-      Result.innerHTML += `</br>${e.currentTarget.result}</br>` ;
-    })
-  } else if (ar.type.includes("image")) {
-    reader.readAsDataURL(ar);
-    reader.addEventListener("load", ()=> {
-      let url = URL.createObjectURL(ar);
-      let img = document.createElement("img");
-      img.setAttribute("src",url);
-      Result.appendChild(img);
-    })
-  } else if (ar.type.includes("video")) {
-    reader.readAsArrayBuffer(ar);
-    reader.addEventListener("load", e => {
-      let video = new Blob([new Uint8Array(e.currentTarget.result)],{type: 'video/mp4'});
-      let url = URL.createObjectURL(video);
-      let vid = document.createElement("video");
-      vid.setAttribute("src",url);
-      vid.setAttribute("autoplay", "autoplay");
-      vid.setAttribute("controls", "controls");
-      Result.appendChild(vid);
-      vid.play();
+  for (let i = 0; i < ar.length; i++) {
+    const reader = new FileReader();
+    if (ar[i].type.includes("text")) {
+      reader.readAsText(ar[i]);
+      reader.addEventListener("load", e => {
+        Result.innerHTML += `</br>${e.currentTarget.result}</br>` ;
+      })
+    } else if (ar[i].type.includes("image")) {
+      reader.readAsDataURL(ar[i]);
+      reader.addEventListener("load", ()=> {
+        let url = URL.createObjectURL(ar[i]);
+        let img = document.createElement("img");
+        img.setAttribute("src",url);
+        Result.appendChild(img);
+      })
+    } else if (ar[i].type.includes("video")) {
+      reader.readAsArrayBuffer(ar[i]);
+      reader.addEventListener("load", e => {
+        let video = new Blob([new Uint8Array(e.currentTarget.result)],{type: 'video/mp4'});
+        let url = URL.createObjectURL(video);
+        let vid = document.createElement("video");
+        vid.setAttribute("src",url);
+        vid.setAttribute("autoplay", "autoplay");
+        vid.setAttribute("controls", "controls");
+        Result.appendChild(vid);
+      })
+    }
+    reader.addEventListener("progress", e => {
+      let load = Math.round((e.loaded / ar[i].size) *100);
+      console.log(load)
+      if (load < 100) {
+        DropArea.innerHTML = `${load}%`;
+        DropArea.classList.add("active");
+      } else {
+        DropArea.classList.remove("active");
+        DropArea.innerHTML = "¡Files dropped! ✅";
+        setTimeout(() => {
+          DropArea.innerHTML = "¡Drop more files here! 👇";
+          DropArea.style.color = "var(--white)";
+          DropArea.style.border = "2px dashed var(--darkRed)";
+        }, 1200)
+      }
     })
   }
 }
